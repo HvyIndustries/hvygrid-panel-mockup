3c53a14432d4d759ad10ea4ac0b99cd6648ffba3
nevada_scout <travis@impactmod.com>
2016-02-17 20:15:43 +0000

    Added sticky footer

---
+++ html5.js
@@ -0,0 +1,7 @@
+// Support HTML5 elements in IE8 and below
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('nav');
